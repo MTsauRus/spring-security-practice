77691a268ea472466d26b507d8d4e86ba32ffea9
mtsaurus <qksrurwnsql@naver.com>
2024-10-10 01:52:00 +0900

    spring security basic

--- springsecurity/src/main/java/com/cos/springsecurity/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cos.springsecurity.repository;
-
-import com.cos.springsecurity.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByUsername(String username); // jpa 쿼리 메서드 참고.
-}
